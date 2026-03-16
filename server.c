3c6157eff2667bac6eb1233d3ccf0eb6ec3a7af3
joewang0430 <joewj0430@gmail.com>
2026-03-16 15:27:42 -0400

    simplified sand_all

--- server.c
+++ server.c
@@ -20,15 +20,16 @@ typedef struct {
 } client_info_t;
 
 static int send_all(int sockfd, const void *buf, size_t len) {
-    const char *p = (const char *)buf;
-    size_t sent = 0;
-    while (sent < len) {
-        ssize_t n = send(sockfd, p + sent, len - sent, 0);
-        if (n <= 0) {
-            return -1;
-        }
-        sent += (size_t)n;
-    }
+    // const char *p = (const char *)buf;
+    // size_t sent = 0;
+    // while (sent < len) {
+    //     ssize_t n = send(sockfd, p + sent, len - sent, 0);
+    //     if (n <= 0) {
+    //         return -1;
+    //     }
+    //     sent += (size_t)n;
+    // }
+    send(sockfd, buf, len, 0);
     return 0;
 }
 
