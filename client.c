eff0136855eeb4b21b7561c0acbdda1061855837
joewang0430 <joewj0430@gmail.com>
2026-03-15 23:08:09 -0400

    step2 select

--- client.c
+++ client.c
@@ -4,109 +4,185 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 #include "message.h"
 
-int main(int argc, char *argv[]) {
-    printf("Client started. Type commands (e.g., /login <client ID> <password> <server-IP> <server-port>)\n");
-    
-    char buffer[MAX_DATA];
-    int sockfd = -1;
+static int handle_login_command(const char *buffer, int *sockfd, char *my_client_id) {
+    char client_id[MAX_NAME];
+    char password[MAX_DATA];
+    char server_ip[32];
+    int server_port;
     struct sockaddr_in server_addr;
 
-    while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
-        // Remove trailing newline
-        buffer[strcspn(buffer, "\n")] = 0;
+    if (sscanf(buffer, "/login %127s %1023s %31s %d", client_id, password, server_ip, &server_port) != 4) {
+        printf("Error: Invalid login format. Usage: /login <client ID> <password> <server-IP> <server-port>\n");
+        return 0;
+    }
 
-        if (strncmp(buffer, "/login", 6) == 0) {
-            char client_id[MAX_NAME];
-            char password[MAX_DATA];
-            char server_ip[32];
-            int server_port;
+    if (*sockfd != -1) {
+        printf("Already connected. Use /logout first.\n");
+        return 0;
+    }
 
-            // Parse login command
-            if (sscanf(buffer, "/login %s %s %s %d", client_id, password, server_ip, &server_port) != 4) {
-                printf("Error: Invalid login format. Usage: /login <client ID> <password> <server-IP> <server-port>\n");
-                continue;
+    *sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (*sockfd < 0) {
+        perror("Socket creation failed");
+        *sockfd = -1;
+        return 0;
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);
+
+    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
+        printf("Invalid address or address not supported\n");
+        close(*sockfd);
+        *sockfd = -1;
+        return 0;
+    }
+
+    printf("Connecting to %s:%d...\n", server_ip, server_port);
+    if (connect(*sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        close(*sockfd);
+        *sockfd = -1;
+        return 0;
+    }
+
+    struct message msg;
+    memset(&msg, 0, sizeof(msg));
+    msg.type = LOGIN;
+    strncpy((char *)msg.source, client_id, MAX_NAME - 1);
+    strncpy((char *)msg.data, password, MAX_DATA - 1);
+    msg.size = strlen((char *)msg.data);
+
+    if (send(*sockfd, &msg, sizeof(msg), 0) < 0) {
+        perror("Send failed");
+        close(*sockfd);
+        *sockfd = -1;
+        return 0;
+    }
+
+    strncpy(my_client_id, client_id, MAX_NAME - 1);
+    my_client_id[MAX_NAME - 1] = '\0';
+    printf("Login request sent. Waiting for server response...\n");
+    return 1;
+}
+
+static void send_exit_if_connected(int sockfd, const char *client_id) {
+    if (sockfd == -1) {
+        return;
+    }
+
+    struct message msg;
+    memset(&msg, 0, sizeof(msg));
+    msg.type = EXIT;
+    strncpy((char *)msg.source, client_id, MAX_NAME - 1);
+    msg.size = 0;
+    send(sockfd, &msg, sizeof(msg), 0);
+}
+
+int main(int argc, char *argv[]) {
+    (void)argc;
+    (void)argv;
+
+    printf("Client started. Type commands (e.g., /login <client ID> <password> <server-IP> <server-port>)\n");
+
+    char buffer[MAX_DATA];
+    int sockfd = -1;
+    int logged_in = 0;
+    char my_client_id[MAX_NAME] = {0};
+
+    while (1) {
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(STDIN_FILENO, &readfds);
+        int max_fd = STDIN_FILENO;
+
+        if (sockfd != -1) {
+            FD_SET(sockfd, &readfds);
+            if (sockfd > max_fd) {
+                max_fd = sockfd;
+            }
+        }
+
+        if (select(max_fd + 1, &readfds, NULL, NULL, NULL) < 0) {
+            perror("select failed");
+            continue;
+        }
+
+        if (FD_ISSET(STDIN_FILENO, &readfds)) {
+            if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
+                send_exit_if_connected(sockfd, my_client_id);
+                if (sockfd != -1) {
+                    close(sockfd);
+                }
+                printf("Input closed. Exiting...\n");
+                break;
             }
 
-            // Create socket
-            if (sockfd != -1) {
-                printf("Already connected! Logout first.\n");
-                continue;
-            }
+            buffer[strcspn(buffer, "\n")] = '\0';
 
-            if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-                perror("Socket creation failed");
-                continue;
-            }
-
-            // Prepare server address
-            memset(&server_addr, 0, sizeof(server_addr));
-            server_addr.sin_family = AF_INET;
-            
-            // Convert port
-            server_addr.sin_port = htons(server_port);
-            
-            // Convert IP
-            if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
-                printf("Invalid address/ Address not supported \n");
-                close(sockfd);
-                sockfd = -1;
-                continue;
-            }
-
-            // Connect
-            printf("Connecting to %s:%d...\n", server_ip, server_port);
-            if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-                perror("Connection failed");
-                close(sockfd);
-                sockfd = -1;
-                continue;
-            }
-            printf("Connected!\n");
-
-            // Send LOGIN message
-            struct message msg;
-            msg.type = LOGIN;
-            strncpy((char*)msg.source, client_id, MAX_NAME);
-            strncpy((char*)msg.data, password, MAX_DATA);
-            msg.size = strlen(password);
-
-            if (send(sockfd, &msg, sizeof(msg), 0) < 0) {
-                perror("Send failed");
-                close(sockfd);
-                sockfd = -1;
-                continue;
-            }
-            printf("Login request sent.\n");
-
-            // Wait for response (Phase 1: Blocking wait for ACK)
-            struct message response;
-            int n = recv(sockfd, &response, sizeof(response), 0);
-            if (n > 0) {
-                if (response.type == LO_ACK) {
-                    printf("Login successful: %s\n", response.data);
-                } else if (response.type == LO_NAK) {
-                    printf("Login failed: %s\n", response.data);
+            if (strncmp(buffer, "/login", 6) == 0) {
+                if (handle_login_command(buffer, &sockfd, my_client_id)) {
+                    logged_in = 0;
+                }
+            } else if (strcmp(buffer, "/logout") == 0) {
+                if (sockfd == -1 || !logged_in) {
+                    printf("You are not logged in.\n");
+                } else {
+                    send_exit_if_connected(sockfd, my_client_id);
                     close(sockfd);
                     sockfd = -1;
-                } else {
-                    printf("Unexpected response: %d\n", response.type);
+                    logged_in = 0;
+                    memset(my_client_id, 0, sizeof(my_client_id));
+                    printf("Logged out.\n");
                 }
+            } else if (strcmp(buffer, "/quit") == 0) {
+                send_exit_if_connected(sockfd, my_client_id);
+                if (sockfd != -1) {
+                    close(sockfd);
+                }
+                printf("Exiting...\n");
+                break;
+            } else if (buffer[0] == '\0') {
+                continue;
             } else {
-                perror("Receive failed or server closed connection");
+                printf("Command not implemented yet in this phase.\n");
+            }
+        }
+
+        if (sockfd != -1 && FD_ISSET(sockfd, &readfds)) {
+            struct message response;
+            memset(&response, 0, sizeof(response));
+            int n = recv(sockfd, &response, sizeof(response), 0);
+            if (n <= 0) {
+                printf("Server disconnected.\n");
                 close(sockfd);
                 sockfd = -1;
+                logged_in = 0;
+                memset(my_client_id, 0, sizeof(my_client_id));
+                continue;
             }
-        
-        } else if (strcmp(buffer, "/quit") == 0) {
-            if (sockfd != -1) {
+
+            response.source[MAX_NAME - 1] = '\0';
+            response.data[MAX_DATA - 1] = '\0';
+
+            if (response.type == LO_ACK) {
+                logged_in = 1;
+                printf("Login successful: %s\n", response.data);
+            } else if (response.type == LO_NAK) {
+                printf("Server response (NAK): %s\n", response.data);
                 close(sockfd);
+                sockfd = -1;
+                logged_in = 0;
+                memset(my_client_id, 0, sizeof(my_client_id));
+            } else if (response.type == MESSAGE) {
+                printf("[%s] %s\n", response.source, response.data);
+            } else {
+                printf("Received message type %u: %s\n", response.type, response.data);
             }
-            printf("Exiting...\n");
-            break;
-        } else {
-            printf("Unknown command or not implemented yet.\n");
         }
     }
     
--- server.c
+++ server.c
@@ -4,7 +4,53 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/select.h>
 #include "message.h"
+
+#define MAX_CLIENTS 64
+
+typedef struct {
+    int sockfd;
+    int logged_in;
+    char client_id[MAX_NAME];
+} client_info_t;
+
+static int is_valid_credential(const char *client_id, const char *password) {
+    if ((strcmp(client_id, "user1") == 0 && strcmp(password, "pass123") == 0) ||
+        (strcmp(client_id, "jill") == 0 && strcmp(password, "eW94dsol") == 0) ||
+        (strcmp(client_id, "jack") == 0 && strcmp(password, "432wlFd") == 0)) {
+        return 1;
+    }
+    return 0;
+}
+
+static int is_client_id_online(client_info_t clients[], const char *client_id) {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].sockfd > 0 && clients[i].logged_in && strcmp(clients[i].client_id, client_id) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+static int add_client(client_info_t clients[], int sockfd) {
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+        if (clients[i].sockfd == 0) {
+            clients[i].sockfd = sockfd;
+            clients[i].logged_in = 0;
+            memset(clients[i].client_id, 0, sizeof(clients[i].client_id));
+            return i;
+        }
+    }
+    return -1;
+}
+
+static void remove_client(client_info_t clients[], int index) {
+    close(clients[index].sockfd);
+    clients[index].sockfd = 0;
+    clients[index].logged_in = 0;
+    memset(clients[index].client_id, 0, sizeof(clients[index].client_id));
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -18,6 +64,8 @@ int main(int argc, char *argv[]) {
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len;
     int port = atoi(argv[1]);
+    client_info_t clients[MAX_CLIENTS];
+    memset(clients, 0, sizeof(clients));
 
     // Create socket
     if ((server_sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -51,38 +99,102 @@ int main(int argc, char *argv[]) {
 
     printf("Listening for connections...\n");
 
-    // Main loop to accept connections
     while (1) {
-        client_len = sizeof(client_addr);
-        if ((client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_addr, &client_len)) < 0) {
-            perror("Accept failed");
+        fd_set readfds;
+        FD_ZERO(&readfds);
+        FD_SET(server_sockfd, &readfds);
+        int max_fd = server_sockfd;
+
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (clients[i].sockfd > 0) {
+                FD_SET(clients[i].sockfd, &readfds);
+                if (clients[i].sockfd > max_fd) {
+                    max_fd = clients[i].sockfd;
+                }
+            }
+        }
+
+        if (select(max_fd + 1, &readfds, NULL, NULL, NULL) < 0) {
+            perror("select failed");
             continue;
         }
 
-        printf("Accepted connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-
-        // Receive Login Message (Blocking for now, just for testing Phase 1)
-        struct message msg;
-        int n = recv(client_sockfd, &msg, sizeof(msg), 0);
-        if (n > 0) {
-            printf("Received message type: %d, size: %d, source: %s\n", msg.type, msg.size, msg.source);
-            if (msg.type == LOGIN) {
-                printf("Login attempt with data (password): %s\n", msg.data);
-                
-                // TODO: Authenticate user here
-                // For now, simple ACK
-                struct message ack;
-                ack.type = LO_ACK;
-                strcpy((char *)ack.data, "Welcome!");
-                ack.size = strlen((char *)ack.data);
-                send(client_sockfd, &ack, sizeof(ack), 0);
-                printf("Sent LO_ACK\n");
+        if (FD_ISSET(server_sockfd, &readfds)) {
+            client_len = sizeof(client_addr);
+            client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_addr, &client_len);
+            if (client_sockfd < 0) {
+                perror("Accept failed");
+            } else {
+                int index = add_client(clients, client_sockfd);
+                if (index < 0) {
+                    printf("Server full. Rejecting new connection.\n");
+                    close(client_sockfd);
+                } else {
+                    printf("Accepted connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                }
             }
-        } else {
-            printf("Client disconnected or error\n");
         }
-        
-        close(client_sockfd); // Close for now in Phase 1 simple test
+
+        for (int i = 0; i < MAX_CLIENTS; i++) {
+            if (clients[i].sockfd <= 0) {
+                continue;
+            }
+
+            if (!FD_ISSET(clients[i].sockfd, &readfds)) {
+                continue;
+            }
+
+            struct message msg;
+            memset(&msg, 0, sizeof(msg));
+            int n = recv(clients[i].sockfd, &msg, sizeof(msg), 0);
+            if (n <= 0) {
+                printf("Client disconnected: %s\n", clients[i].logged_in ? clients[i].client_id : "(not logged in)");
+                remove_client(clients, i);
+                continue;
+            }
+
+            msg.source[MAX_NAME - 1] = '\0';
+            msg.data[MAX_DATA - 1] = '\0';
+
+            if (msg.type == LOGIN) {
+                struct message response;
+                memset(&response, 0, sizeof(response));
+                const char *client_id = (const char *)msg.source;
+                const char *password = (const char *)msg.data;
+
+                printf("Login attempt: id=%s\n", client_id);
+
+                if (clients[i].logged_in) {
+                    response.type = LO_NAK;
+                    snprintf((char *)response.data, MAX_DATA, "Already logged in on this connection");
+                } else if (is_client_id_online(clients, client_id)) {
+                    response.type = LO_NAK;
+                    snprintf((char *)response.data, MAX_DATA, "Client ID already online");
+                } else if (!is_valid_credential(client_id, password)) {
+                    response.type = LO_NAK;
+                    snprintf((char *)response.data, MAX_DATA, "Invalid client ID or password");
+                } else {
+                    response.type = LO_ACK;
+                    snprintf((char *)response.data, MAX_DATA, "Welcome!");
+                    clients[i].logged_in = 1;
+                    strncpy(clients[i].client_id, client_id, MAX_NAME - 1);
+                    clients[i].client_id[MAX_NAME - 1] = '\0';
+                }
+
+                response.size = strlen((char *)response.data);
+                send(clients[i].sockfd, &response, sizeof(response), 0);
+            } else if (msg.type == EXIT) {
+                printf("Exit request from %s\n", clients[i].logged_in ? clients[i].client_id : "(not logged in)");
+                remove_client(clients, i);
+            } else {
+                struct message response;
+                memset(&response, 0, sizeof(response));
+                response.type = LO_NAK;
+                snprintf((char *)response.data, MAX_DATA, "Unsupported message in current phase");
+                response.size = strlen((char *)response.data);
+                send(clients[i].sockfd, &response, sizeof(response), 0);
+            }
+        }
     }
 
     close(server_sockfd);
